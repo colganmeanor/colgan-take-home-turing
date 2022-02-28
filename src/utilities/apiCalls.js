936a400a6251d345666b36713ec6c0ec8667afcc
Colgan Meanor <colganmeanor7@gmail.com>
2022-02-28 16:43:03 -0600

    Add function to load articles for home page

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -1,1 +1,16 @@
 const key = '0JUSpi2WsHgz1Ni61wAut6y1KVMhXLRA'
+
+export const apiCalls = {
+    homePageLoad: () => {
+        return fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${key}`)
+            .then(res => {
+                if(res.ok) {
+                    return res.json()
+                } else {
+                    throw new Error()
+                }
+            })
+            .catch(err => err)
+    },
+
+}
