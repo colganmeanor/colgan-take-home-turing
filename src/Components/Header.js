cdb5f89be17ad2a5e45ed90aac3091de5bbfbd14
Colgan Meanor <colganmeanor7@gmail.com>
2022-03-01 10:51:30 -0600

    Add basic structure to Header component

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export const Header = () => {
+
+    return (
+        <div className="header">
+            <div className="header-top-container">
+            <p>Tuesday, March 1st, 2022</p>
+            <h1>The Colgan Times</h1>
+                <div className="header-buttons">
+                    <button>Log In</button>
+                    <button>Subscribe</button>
+                </div>
+            </div>
+        </div>
+    )
+
+}
