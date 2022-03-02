924b37b4ab6894c5f5ff815cd6801ee04b55ba83
Colgan Meanor <colganmeanor7@gmail.com>
2022-03-01 19:04:43 -0600

    Add placeholder info to ArticleDetail Component and export it

---
+++ src/Components/ArticleDetail.js
@@ -0,0 +1,12 @@
+import '../Styles/ArticleDetail.css';
+
+
+export const ArticleDetail = ({ article }) => {
+
+    return(
+        <div className='article-detail'>
+            <p>This will be the article detail component</p>
+        </div>
+    )
+
+}
